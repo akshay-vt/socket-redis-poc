38d9960621faee4dddebaa0c7a76bf4a3ecf5051
Akshay Singla <akshay.singla@volumetree.com>
2023-11-23 14:42:21 +0530

    fix

--- index.js
+++ index.js
@@ -12,37 +12,37 @@ const subClient = pubClient.duplicate();
 
 
 Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
-    pubClient.GET('name').then(val=>console.log('pubClient:: ',val))
-    subClient.GET('name').then(val=>console.log('subClient:: ',val))
-  });
+    pubClient.GET('name').then(val=>console.log('pubClient:: ',val));
 
-io.adapter(createAdapter(pubClient, subClient));
+    io.adapter(createAdapter(pubClient, subClient));
 
-io.use(function (socket, next) {
-    const socketId = socket.handshake.query.user_id;
-    console.log('middelware: ', socketId)
-    socket.id = socketId
-    next()
-});
-
-io.on("connection", (socket) => {
-    console.log(`A client with socket id ${socket.id} connected!`);
-
-    socket.on("disconnect", () => {
-        console.log("Socket disconnected!", socket.id);
+    io.use(function (socket, next) {
+        const socketId = socket.handshake.query.user_id;
+        console.log('middelware: ', socketId)
+        socket.id = socketId
+        next()
     });
 
-    socket.on("server:message", (data) => {
-        console.log('Recieve message : ', JSON.stringify(data),  ' ',socket.id)
-        let targetClientId = data.toId
-        console.log('targetClientId: ',targetClientId)
-        console.log('Socket id is not conencted here : ', !!io.sockets.sockets.get(targetClientId))
-        // if (!!io.sockets.sockets.get(targetClientId)) {
-        // }
-        io.to(targetClientId).emit("client:message", "Message is sent by: " + socket.id);
+    io.on("connection", (socket) => {
+        console.log(`A client with socket id ${socket.id} connected!`);
+
+        socket.on("disconnect", () => {
+            console.log("Socket disconnected!", socket.id);
+        });
+
+        socket.on("server:message", (data) => {
+            console.log('Recieve message : ', JSON.stringify(data),  ' ',socket.id)
+            let targetClientId = data.toId
+            console.log('targetClientId: ',targetClientId)
+            console.log('Socket id is not conencted here : ', !!io.sockets.sockets.get(targetClientId))
+            // if (!!io.sockets.sockets.get(targetClientId)) {
+            // }
+            io.to(targetClientId).emit("client:message", "Message is sent by: " + socket.id);
+        });
     });
+
+    io.listen(3000);
+
+    console.log('IO connected success');
+
 });
-
-io.listen(3000);
-
-console.log('IO connected success')
